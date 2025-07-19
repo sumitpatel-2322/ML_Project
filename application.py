09d0271062507ff1379e7e1943a1a54fee2999c4
Sumit Patel <sumitpatel2907@gmail.com>
2025-07-19 20:31:25 +0530

    renamed app.run to application.run

--- application.py
+++ application.py
@@ -37,5 +37,5 @@ def predict_datapoint():
     
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
     print("Server Running on http://localhost:3000")
